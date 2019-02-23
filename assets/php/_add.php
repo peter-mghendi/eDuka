8f330b0dfbc13614c795e8b70b2c92cdf744c811
TypicalGeek <plenjo15@gmail.com>
2019-02-23 15:25:29 +0300

    Updated wishlist functionality
    Added ability to remove from wishlist.

--- assets/php/_add.php
+++ assets/php/_add.php
@@ -3,7 +3,14 @@
     if(isset($_GET['list'])){
         extract($_GET);
         if ($list=="wishlist"){
-            $wish_query = "INSERT INTO wishlist VALUES(NULL, '$user', '$product')";
+            $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
+            $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
+            $wished_count = mysqli_num_rows($wished_result);
+            if ($wished_count == 0){
+                $wish_query = "INSERT INTO wishlist VALUES(NULL, '$user', '$product')";
+            } else {
+                $wish_query = "DELETE FROM wishlist WHERE user = '$user' AND product = '$product'";
+            }
             mysqli_query($db, $wish_query);
             header("location: index.php");
         }
