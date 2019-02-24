1cc1f20077bf88d09568a46f39999d93b2431614
TypicalGeek <plenjo15@gmail.com>
2019-02-24 17:10:31 +0300

    Minify product sets
    Added for loop for product sets.

--- assets/php/content.php
+++ assets/php/content.php
@@ -1,10 +1,5 @@
-<?php $slides = 4; $num=4; $user = $_SESSION['token'];
-#$images = array("assets/images/smartHD.jpg", "assets/images/flexiblekyb.jpg", "assets/images/IdeapadLarge.jpg", "assets/images/flexiblekyb.jpg"); 
-#$titles = array("HD Monitor", "Flexiboard", "Lappytop", "Flexiboard Again");
-#$descriptions = array("View all your movies and games in ultra sharp Full HD quality", "Roll it up and take it with you wherever you go", "The ultimate students' laptop, built to cater to your every need", "I already discussed this one. Roll it up...");
-?>
+<?php $slides = 4; $num=4; $user = $_SESSION['token']; $productSets=array("new", "popular", "monthly", "flash"); ?>
 <main role="main">
-
   <div class="container margin-vertical">
     <div id="myCarousel" class="carousel slide bg-dark" data-ride="carousel">
       <ol class="carousel-indicators">
@@ -16,11 +11,8 @@
           if(!in_array("featured", $categories)) continue;
           $state = ($i==0)?"active":"";
           echo "<li data-target='#myCarousel' data-slide-to='$i' class='$state'></li>";
-          $i++;
-      } ?>
+          $i++;}?>
       </ol>
-
-      <!-- Wrapper for slides -->
       <div class="carousel-inner" role="listbox">
         <?php $i = 0;
         $set_query = "SELECT * FROM products";
@@ -34,16 +26,10 @@
             <h5 class='list-price text-danger'>Was: <s>Kshs. $set_row[5]</s></h5> <h4>Now: Ksh. $set_row[6]</h4></div></div>"; 
             $i++; }?>
       </div>
-
-      <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-      </a>
-      <a class="carousel-control-next" href="#myCarousel" data-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </a>
+      <a class="carousel-control-prev" href="#myCarousel" data-slide="prev"><span class="carousel-control-prev-icon"></span></a>
+      <a class="carousel-control-next" href="#myCarousel" data-slide="next"><span class="carousel-control-next-icon"></span></a>
     </div>
   </div>
-
   <nav class="container secondary-nav">
     <ul class="nav justify-content-center">
       <li class="nav-item"><a href="#new" class="nav-link">New</a></li>
@@ -53,37 +39,30 @@
       <li class="nav-item"><a href="#flash" class="nav-link">Flash Sales</a></li>
     </ul>
   </nav>
+
   <div class="album py-5 bg-light">
     <div class="container">
-      <div class="container-fluid product-set" id="new">
-        <div class="d-flex">
-          <h3>New Items</h3>
-          <h3 class="ml-auto"><small><a href="#">SEE MORE >></a></small></h3>
-        </div>
-        <hr>
-        <div class="row">
-          <?php 
-            $i = 0;
-            $set_query = "SELECT * FROM products";
-            $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
-            while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
-              $categories = explode(";", $set_row[7]);
-              $product = $set_row[1];
-              $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
-              $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
-              $wished_count = mysqli_num_rows($wished_result);
-              $wished = ($wished_count == 1)?"fa":"far";
-              $wished_status = ($wished_count == 1)?"Added":"Add";
-              if(!in_array("new", $categories)) continue;
-              include 'assets/php/product.php';
-              include 'assets/php/details.php';
-              $i++;
-            } ?>
-        </div>
-      </div>
 
+    <?php for ($x=0; $x<4; $x++){
+      echo "<div class='container-fluid product-set' id='new'><div class='d-flex'><h3>New Items</h3><h3 class='ml-auto'><small><a href='#'>SEE MORE >></a></small></h3></div><hr><div class='row'>";
+      $i = 0;
+      $set_query = "SELECT * FROM products";
+      $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
+      while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
+        $categories = explode(";", $set_row[7]);
+        $product = $set_row[1];
+        $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
+        $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
+        $wished_count = mysqli_num_rows($wished_result);
+        $wished = ($wished_count == 1)?"fa":"far";
+        $wished_status = ($wished_count == 1)?"Added":"Add";
+        if(!in_array("new", $categories)) continue;
+        include 'assets/php/product.php';
+        include 'assets/php/details.php';
+        $i++;}
+      echo "</div></div>"; }?>
 
-      <div class="container-fluid product-set" id="popular">
+      <!-- <div class="container-fluid product-set" id="popular">
         <div class="d-flex">
           <h3>Popular Items</h3>
           <h3 class="ml-auto"><small><a href="#">SEE MORE >></a></small></h3>
@@ -91,25 +70,25 @@
         <hr>
         <div class="row">
           <?php 
-        $i = 0;
-        $set_query = "SELECT * FROM products";
-        $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
-        while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
-          $categories = explode(";", $set_row[7]);
-          $product = $set_row[1];
-          $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
-          $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
-          $wished_count = mysqli_num_rows($wished_result);
-          $wished = ($wished_count == 1)?"fa":"far";
-          if(!in_array("popular", $categories)) continue;
-          include 'assets/php/product.php';
-          include 'assets/php/details.php';
-          $i++;
-        } ?>
+        // $i = 0;
+        // $set_query = "SELECT * FROM products";
+        // $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
+        // while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
+        //   $categories = explode(";", $set_row[7]);
+        //   $product = $set_row[1];
+        //   $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
+        //   $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
+        //   $wished_count = mysqli_num_rows($wished_result);
+        //   $wished = ($wished_count == 1)?"fa":"far";
+        //   if(!in_array("popular", $categories)) continue;
+        //   include 'assets/php/product.php';
+        //   include 'assets/php/details.php';
+        //   $i++;
+        // } ?>
         </div>
-      </div>
+      </div> -->
 
-      <div class="container-fluid product-set" id="monthly">
+      <!-- <div class="container-fluid product-set" id="monthly">
         <div class="d-flex">
           <h3>
             <?php echo date("F");?> Sales</h3>
@@ -118,25 +97,25 @@
         <hr>
         <div class="row" id="flash">
           <?php 
-        $i = 0;
-        $set_query = "SELECT * FROM products";
-        $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
-        while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
-          $categories = explode(";", $set_row[7]);
-          $product = $set_row[1];
-          $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
-          $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
-          $wished_count = mysqli_num_rows($wished_result);
-          $wished = ($wished_count == 1)?"fa":"far";
-          if(!in_array("monthly", $categories)) continue;
-          include 'assets/php/product.php';
-          include 'assets/php/details.php';
-          $i++;
-        } ?>
+        // $i = 0;
+        // $set_query = "SELECT * FROM products";
+        // $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
+        // while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
+        //   $categories = explode(";", $set_row[7]);
+        //   $product = $set_row[1];
+        //   $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
+        //   $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
+        //   $wished_count = mysqli_num_rows($wished_result);
+        //   $wished = ($wished_count == 1)?"fa":"far";
+        //   if(!in_array("monthly", $categories)) continue;
+        //   include 'assets/php/product.php';
+        //   include 'assets/php/details.php';
+        //   $i++;
+        // } ?>
         </div>
-      </div>
+      </div> -->
 
-      <div class="container-fluid product-set" id="flash">
+      <!-- <div class="container-fluid product-set" id="flash">
         <div class="d-flex">
           <h3>Flash Sales</h3>
           <h3 class="ml-auto"><small><a href="#">SEE MORE >></a></small></h3>
@@ -144,23 +123,23 @@
         <hr>
         <div class="row" id="flash">
           <?php 
-        $i = 0;
-        $set_query = "SELECT * FROM products";
-        $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
-        while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
-          $categories = explode(";", $set_row[7]);
-          $product = $set_row[1];
-          $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
-          $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
-          $wished_count = mysqli_num_rows($wished_result);
-          $wished = ($wished_count == 1)?"fa":"far";
-          if(!in_array("flash", $categories)) continue;
-          include 'assets/php/product.php';
-          include 'assets/php/details.php';
-          $i++;
-        } ?>
+        // $i = 0;
+        // $set_query = "SELECT * FROM products";
+        // $set_result = mysqli_query($db, $set_query) or die(mysqli_error($db));
+        // while(($set_row = mysqli_fetch_row($set_result)) && ($i<$num)){
+        //   $categories = explode(";", $set_row[7]);
+        //   $product = $set_row[1];
+        //   $wished_query = "SELECT * FROM wishlist WHERE user = '$user' AND product = '$product'";
+        //   $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
+        //   $wished_count = mysqli_num_rows($wished_result);
+        //   $wished = ($wished_count == 1)?"fa":"far";
+        //   if(!in_array("flash", $categories)) continue;
+        //   include 'assets/php/product.php';
+        //   include 'assets/php/details.php';
+        //   $i++;
+        // } ?>
         </div>
-      </div>
+      </div> -->
 
     </div>
   </div>
