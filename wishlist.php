5c11e120442982a48a6aa1781e79b40bfeee4049
TypicalGeek <plenjo15@gmail.com>
2019-02-25 06:01:11 +0300

    Created wishlist page
    Implemented wishlist page, pending design.

--- wishlist.php
+++ wishlist.php
@@ -1,4 +1,8 @@
-<?php include 'assets/php/_protect.php'?>
+<?php 
+    include 'assets/php/_protect.php';
+    include 'assets/php/_connect.php';
+    include 'assets/php/account/edit.php';
+    include 'assets/php/_add.php';?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,10 +14,32 @@
 </head>
 <body>
 <?php 
-    include 'assets/php/_nav.php';
-    include 'assets/php/_connect.php';
+   # include 'assets/php/_nav.php';  
+    include 'assets/php/modal.php';
+    $user = $_SESSION['token'];
+    $wished_query = "SELECT * FROM wishlist WHERE user = '$user'";
+    $wished_result = mysqli_query($db, $wished_query) or die(mysqli_error());
 ?>
-    
+    <table class="table table-borderless table-responsive" style="margin: 80px;">
+        <!-- <thead><h5>My Wishlist</h5></thead> -->
+        <tbody>
+        <?php if (mysqli_num_rows($wished_result) > 0) {
+            while($wished_row = mysqli_fetch_row($wished_result)){
+                $product_query = "SELECT * FROM products WHERE token = $wished_row[2]";
+                $product_result = mysqli_query($db, $product_query);
+                $product_row = mysqli_fetch_row($product_result);
+                echo "<tr>
+                    <td><img src='assets/images/products/$product_row[1].jpg' alt='$product_row[2]' style='width: 50%;'></td>
+                    <td>$product_row[2] $product_row[3]</td>
+                    <td>Ksh. $product_row[6]</td>
+                    <td class='set-title'>$product_row[9]</td>
+                    <td class = 'd-flex'><div class='mx-auto'><a href='assets/php/_add.php?&list=wishlist&user=$user&product=$product_row[1]'><span class='fa fa-trash'></span> </a>
+                    <a href='#'> <span class='fa fa-cart-plus'></span></a></div></td>
+                </tr>";}} else echo "<h4>Nothing to show</h4>";?>
+        </tbody>
+    </table>
+    <?php 
+    include 'assets/php/footer.php'; ?>
 <script src="../js/jquery-3.3.1.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
 </body>
