549a7aee2795a9ca3dc7cfe468397df51e657c60
TypicalGeek <plenjo15@gmail.com>
2019-02-25 06:38:46 +0300

    Cart
    Began cart implementation.

--- assets/php/_add.php
+++ assets/php/_add.php
@@ -10,5 +10,18 @@
             else $wish_query = "DELETE FROM wishlist WHERE user = '$user' AND product = '$product'";
             mysqli_query($db, $wish_query);
             header("location: index.php");
+        } elseif ($list=="cart") {
+            if($action == "a"){
+                $cart_query = "SELECT * FROM cart";
+                $cart_result = mysqli_query($db, $cart_query);
+                $cart_count = mysqli_num_rows($cart_result);
+                if ($cart_count == 0){
+                    # ADD
+                } else {
+                    # Update quantity
+                }
+            } elseif ($action == "r") {
+                # Delete
+            }
         }
-    }
+    
--- wishlist.php
+++ wishlist.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 <?php 
-   # include 'assets/php/_nav.php';  
+    include 'assets/php/_nav.php';  
     include 'assets/php/modal.php';
     $user = $_SESSION['token'];
     $wished_query = "SELECT * FROM wishlist WHERE user = '$user'";
