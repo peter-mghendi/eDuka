b00faa44a5c1e10d0526d71b4f1a7ce76a57a994
TypicalGeek <plenjo15@gmail.com>
2019-03-14 16:03:00 +0300

    Fixed cart/wishlist bug

--- assets/php/product.php
+++ assets/php/product.php
@@ -8,6 +8,6 @@ echo "<div class='col-md-3'><div class='card mb-3 shadow-sm'>
   <p class='list-price text-danger'>List Price: <s>Kshs. $set_row[5]</s></p><p class='price'>Our Price: Kshs. $set_row[6]</p>       
   <div class='d-flex justify-content-between align-items-center'><div class='mx-auto'>
       <button type='button' class='btn btn-sm btn-success product-btn' data-toggle='modal' data-target='#productModal_$set_row[1]' data-id='$set_row[1]'>View Details</button>
-      <a class='btn btn-sm btn-warning' $wish_attrib><i class='fa fa-cart-plus' aria-hidden='true'></i> Add</a>
-      <a class='btn btn-sm btn-link' $cart_attrib><i class='$wished fa-star' aria-hidden='true'></i></a>
+      <a class='btn btn-sm btn-warning' $cart_attrib><i class='fa fa-cart-plus' aria-hidden='true'></i> Add</a>
+      <a class='btn btn-sm btn-link' $wish_attrib><i class='$wished fa-star' aria-hidden='true'></i></a>
   </div></div></div></div></div>";
